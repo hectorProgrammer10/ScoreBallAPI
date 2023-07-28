f63a954f57b7d90fd0aa674e3457957a8948c656
LuisAlbertoB <luisalbagoz@gmail.com>
2023-07-27 19:17:36 -0600

    first commit

--- consumer.js
+++ consumer.js
@@ -2,22 +2,22 @@ const amqp = require('amqplib')
 
 const rabbitSettings = {
     protocol: 'amqp',
-    hostname: 'localhost',
+    hostname: '52.71.43.19',
     port: 5672,
     username: 'guest',
     password: 'guest'
 }
 async function connect() {
-    const queue = "estafeta"
+    const queue = "score"
     try {
         const conn = await amqp.connect(rabbitSettings);
         console.log('Conexión exitosa')
-        const channel = await conn.createChannel()
+        const channel3 = await conn.createChannel()
         console.log ("Canal creado exitosamente")
 
-        channel.consume(queue, (msn)=> {
-            console.log(msn.content.toString())
-            channel.ack(msn)
+        channel3.consume(queue, (msn)=> {
+            console.log('Tu puntuacion es:', msn.content.toString())
+            channel3.ack(msn)
         })
         
 
